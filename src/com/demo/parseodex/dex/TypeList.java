8e8468ba05a597d04a585fa729efb5b76b393bf8
Tracy Jin <1032689332@qq.com>
2017-04-05 16:08:25 +0800

    增加dex格式官方说明html

--- src/com/demo/parseodex/dex/TypeList.java
+++ src/com/demo/parseodex/dex/TypeList.java
@@ -4,16 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TypeList {
-	
-	/**
-	 * struct type_list
-		{
-		uint size;
-		ushort type_idx[size];
-		}
-	 */
-	
-	public int size;//�����ĸ���
-	public List<Short> type_idx = new ArrayList<Short>();//����������
-	
+
+    /**
+     * struct type_list { uint size; ushort type_idx[size]; }
+     */
+
+    public int size;// 参数的个数
+    public List<Short> type_idx = new ArrayList<Short>();// 参数的类型
+
 }
